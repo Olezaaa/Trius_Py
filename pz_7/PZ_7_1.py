9ac20781d1acd33be64c9b4fe0f6e4433dbe368b
Leksusss <leksinaulya@gmail.com>
2025-02-08 16:12:21 +0300

    сделал пз3

---
+++ pz_7/PZ_7_1.py
@@ -0,0 +1,9 @@
+#Дан символ C и строки S, S0. Перед каждым вхождением символа C в строку S
+#вставить строку S0.
+def insert_string(c, S, S0):
+    return S.replace(c, S0 + c)
+c = 'a'
+S = 'Toros'
+S0 = 'x'
+result = insert_string(c, S, S0)
+print(result)
