04c819e3259402155a16fef13f04077875227ac1
Olezaaa <triusolegtrius841@gmail.com>
2025-06-25 22:43:04 +0300

    сделал пз 13

--- pz_13/PZ_13_1.py
+++ pz_13/PZ_13_1.py
@@ -1,1 +1,13 @@
 #В двумерном списке элементы последней строки заменить на 0
+
+import random
+rows = 3 #столбцы
+cols = 3 #строки
+matrix = [[random.randint(1, 10) for j in range(cols)] for i in range(rows)]
+print("Изначальная матрица: ")
+for row in matrix:
+    print(row)
+matrix[-1] = [0 for _ in range(cols)]
+print("Изменённая матрица: ")
+for row in matrix:
+    print(row)
--- pz_13/PZ_13_2.py
+++ pz_13/PZ_13_2.py
@@ -1,1 +1,17 @@
 #В двумерном списке элементы столбца N (N задать с клавиатуры) увеличить в два раза.
+
+import random
+rows = 3
+cols = 3
+matrix = [[random.randint(1, 10) for j in range(cols)] for i in range(rows)]
+print("Изначальная матрица: ")
+for row in matrix:
+    print(row)
+number = int(input("Введите номер столбца для увеличения в два раза: "))
+#увеличение элементов столбца в два раза
+matrix = list(map(lambda row, idx: [elem * 2 if idx == number - 1 else elem for idx, elem in enumerate(row)], matrix, range(len(matrix))))
+#for i in range(len(matrix)):
+#    matrix[i][number - 1] *= 2
+print("Изменённая матрица: ")
+for row in matrix:
+    print(row)
